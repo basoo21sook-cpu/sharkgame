efe1df4072069af66ed890a4d63a66e18c014f41
basoo21sook-cpu <basoo21sook@gmail.com>
2025-11-19 00:02:18 +0900

    12-2,3

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "board.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
@@ -14,8 +15,20 @@ int main(int argc, char *argv[]) {
     printf("*****************************************************************\n");
     printf("=================================================================\n\n\n");
 
-    printf("\n\n\n\n\n\n\n");
+	//step 1. initializaion (player name setting, variables)
+	
+	//step 2. turn play (do-while)
+	// 2-1. status printing
+	// 2-2. roll die
+	// 2-3. move (result)
+	// 2-4. change turn, shark move
+	
+	// step 3. game end (winner printing)
+
+
+		
 	//ending
+    printf("\n\n\n\n\n\n\n");
     printf("=================================================================\n");
     printf("*****************************************************************\n");
     printf("*                        CONGRATULATION!!!!!                    *\n");
